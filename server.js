134c74d9f1c00e7bf806a3afac3548b7af635d9f
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-10-10 14:18:02 +0100

    git update

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path')
 const cors = require('cors')
 const { logger } = require('./middleware/logEvents')
 const errorHandler = require('./middleware/errorHandler')
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3500;
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json());
@@ -89,20 +89,20 @@ const cohort4 = (req, res) => {
 
 app.get('/big-devs(.html)?', [cohort1, cohort2, cohort3, cohort4])
 
-// app.get('/*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'views', '404.html'))
-// })
-
-app.all('*', (req, res) => {
-    res.status(404);
-    if (req.accepts(html)) {
-        res.sendFile(path.join(__dirname, 'views', '404.html'));
-    } else if (req.accepts('json')) {
-        res.json({ 'error': '404 Not Found' })
-    } else {
-        res.type('txt').send('404 Not Found')
-    }
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', '404.html'))
 })
+
+// app.all('*', (req, res) => {
+//     res.status(404);
+//     if (req.accepts(html)) {
+//         res.sendFile(path.join(__dirname, 'views', '404.html'));
+//     } else if (req.accepts('json')) {
+//         res.json({ 'error': '404 Not Found' })
+//     } else {
+//         res.type('txt').send('404 Not Found')
+//     }
+// })
 
 app.use(errorHandler)
 app.listen(PORT, () => console.log(`server running on port ${PORT}`))
